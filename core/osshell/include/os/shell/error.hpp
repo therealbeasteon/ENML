ffafce260b0a1991357e20d742291d6dd5ae22c5
therealbeasteon <eonx1904@gmail.com>
2026-08-10 11:04:13 -0400

    M4.1 add trusted chrome lifecycle errors

--- core/osshell/include/os/shell/error.hpp
+++ core/osshell/include/os/shell/error.hpp
@@ -21,6 +21,9 @@ inline constexpr std::uint32_t invalid_activation_intent = 11U;
 inline constexpr std::uint32_t stale_activation_intent = 12U;
 inline constexpr std::uint32_t preview_capture_denied = 13U;
 inline constexpr std::uint32_t stale_preview_grant = 14U;
+inline constexpr std::uint32_t invalid_chrome_lease = 15U;
+inline constexpr std::uint32_t stale_chrome_lease = 16U;
+inline constexpr std::uint32_t chrome_authority_denied = 17U;
 } // namespace errors
 
 [[nodiscard]] constexpr os::core::Error shell_error(std::uint32_t code) noexcept {
