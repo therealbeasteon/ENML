c48ca3f998221f9428dc590c203a850180aa9959
therealbeasteon <eonx1904@gmail.com>
2026-08-12 16:43:13 -0400

    M7.8.1 fix explicit Result assertions in translation tests

--- tests/unit/kernel/process_translation_test.cpp
+++ tests/unit/kernel/process_translation_test.cpp
@@ -8,7 +8,10 @@
 #include <cstdlib>
 
 namespace {
-void require(bool value) { if (!value) std::abort(); }
+template <typename T>
+void require(const T& value) {
+    if (!static_cast<bool>(value)) std::abort();
+}
 }
 
 int main() {
